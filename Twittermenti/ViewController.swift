c077486e0dff16fda6b1bf19b4e9aae0f8c283ac
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2019-01-15 11:23:04 -0600

    Use Twitter API to scrape tweets about Apple

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,16 +7,24 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "", consumerSecret: "")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
     }
 
     @IBAction func predictPressed(_ sender: Any) {
