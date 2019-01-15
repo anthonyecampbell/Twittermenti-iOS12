147721b0a774e93b01afdf60a72afc5b8da9268c
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2019-01-15 09:44:17 -0600

    Add project to GitHub

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Anthony Campbell on 01/15/2019.
+//  Copyright © 2019 Anthony Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
+
