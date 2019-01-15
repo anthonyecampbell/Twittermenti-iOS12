22d3ac241154aac2a0ddf38539ab9ef00a93562f
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2019-01-15 11:48:11 -0600

    Add ability to get a score from the last 100 tweets about Apple

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -15,13 +17,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
-    let swifter = Swifter(consumerKey: "", consumerSecret: "")
+    private let sentimentClassifier = TweetSentimentClaassifier()
+    
+    private let swifter = Swifter(consumerKey: "", consumerSecret: "")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-            print(results)
+            
+            var tweets = [TweetSentimentClaassifierInput]()
+            
+            for i in 0..<100 {
+                if let tweet = results[i]["full_text"].string {
+                    let tweetForClassification = TweetSentimentClaassifierInput(text: tweet)
+                    tweets.append(tweetForClassification)
+                }
+            }
+            
+            do {
+                let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                
+                var sentimentScore = 0
+                
+                for prediction in predictions {
+                    let sentiment = prediction.label
+                    
+                    if sentiment == "Pos" {
+                        sentimentScore += 1
+                    } else if sentiment == "Neg" {
+                        sentimentScore -= 1
+                    }
+                }
+                
+            } catch {
+                print("There was an error with making a prediction, \(error)")
+            }
+           
+            
+            print(tweets)
+            
         }) { (error) in
             print("There was an error with the Twitter API Request, \(error)")
         }
